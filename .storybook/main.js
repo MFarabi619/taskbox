762cc7cb846cfe870dba434685ad904c8b9c9d95
Mumtahin Farabi <mfarabi619@gmail.com>
2024-05-22 00:48:10 -0400

    chore(Storybook): Add accessibility addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,5 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   // stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   stories: ["../src/components/**/*.stories.@(js|jsx)"],
   staticDirs: ["../public"],
@@ -8,6 +7,7 @@ const config = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   framework: {
     name: "@storybook/react-vite",
